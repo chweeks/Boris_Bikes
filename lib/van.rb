38cb75129a7d5f2c35f9a412c1fcf8e7ba13df23
chweeks <chrisharryweeks@gmail.com>
2015-09-04 23:53:05 +0100

    added Van class and van_spec

---
+++ lib/van.rb
@@ -0,0 +1,12 @@
+class Van
+  attr_accessor :rack
+
+  def initialize
+    @rack = []
+  end
+
+  def collect_broken_bikes(station)
+    @rack = station.release_broken_bikes
+    station.remove_broken_bikes
+  end
+end
